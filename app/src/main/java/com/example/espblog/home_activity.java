4ad6e21718e45a7ae30a49a817c25f43a7482215
Cheikhdeme <93883320+Cheikhdeme@users.noreply.github.com>
2023-07-24 14:21:42 +0000

    Initial commit

---
+++ app/src/main/java/com/example/espblog/home_activity.java
@@ -0,0 +1,60 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		home
+	 *	@date 		Saturday 20th of May 2023 09:56:32 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.espblog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class home_activity extends Activity {
+
+	
+	private View _bg__home_ek2;
+	private TextView simpleblog;
+	private View rectangle_1;
+	private TextView article_1;
+	private TextView text_goes_here;
+	private View ellipse_1;
+	private ImageView vector;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+
+		
+		_bg__home_ek2 = (View) findViewById(R.id._bg__home_ek2);
+		simpleblog = (TextView) findViewById(R.id.simpleblog);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		article_1 = (TextView) findViewById(R.id.article_1);
+		text_goes_here = (TextView) findViewById(R.id.text_goes_here);
+		ellipse_1 = (View) findViewById(R.id.ellipse_1);
+		vector = (ImageView) findViewById(R.id.vector);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
